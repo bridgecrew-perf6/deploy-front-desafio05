c15fe1ae2dbe2bc0d8c11fd2d2965aea6709edfd
Bruno <br9uno@gmail.com>
2022-05-31 10:24:34 -0300

    Criação do card

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,18 +1,11 @@
+import NavLink from "../../components/NavLinks";
 import "./style.css";
 
 function Profile() {
 	return (
-		<div class='grid-container'>
-			<div class='grid-item'>1</div>
-			<div class='grid-item'>2</div>
-			<div class='grid-item'>3</div>
-			<div class='grid-item'>4</div>
-			<div class='grid-item'>5</div>
-			<div class='grid-item'>6</div>
-			<div class='grid-item'>7</div>
-			<div class='grid-item'>8</div>
-			<div class='grid-item'>9</div>
-		</div>
+		<navbar className='column ai-center'>
+			<NavLink />
+		</navbar>
 	);
 }
 
