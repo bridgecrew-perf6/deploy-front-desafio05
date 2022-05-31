f0b7b615140d5b84c05e17b8e84ddea8d21f6d11
Bruno <br9uno@gmail.com>
2022-05-31 16:08:09 -0300

    estrutura

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -6,21 +6,23 @@ import "./style.css";
 
 function Profile() {
 	return (
-		<navbar className='column ai-center gap-nav-items'>
-			<NavLink image={HomeImg} text='Home' color='main-pink' />
-			<NavLink
-				image={ProfileImg}
-				text='Clientes'
-				color='color-navbar-text-default'
-				display='hidden'
-			/>
-			<NavLink
-				image={DocumentImg}
-				text='Cobranças'
-				color='color-navbar-text-default'
-				display='hidden'
-			/>
-		</navbar>
+		<div className='row'>
+			<navbar className='column ai-center gap-nav-items'>
+				<NavLink image={HomeImg} text='Home' color='main-pink' />
+				<NavLink
+					image={ProfileImg}
+					text='Clientes'
+					color='color-navbar-text-default'
+					display='hidden'
+				/>
+				<NavLink
+					image={DocumentImg}
+					text='Cobranças'
+					color='color-navbar-text-default'
+					display='hidden'
+				/>
+			</navbar>
+		</div>
 	);
 }
 
