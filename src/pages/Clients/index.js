3c7f9c23d7315cdce12cd644f3af8e7ebc42a068
Franciely Lavine <francielylavine@hotmail.com>
2022-06-01 19:02:04 -0300

    Início da estilização do cliente

--- src/pages/Clients/index.js
+++ src/pages/Clients/index.js
@@ -2,6 +2,10 @@ import DocumentImg from "../../assets/documents.svg";
 import HomeImg from "../../assets/home.svg";
 import ProfileImg from "../../assets/profile.svg";
 import NavLink from "../../components/NavLinks";
+import People from "../../assets/people.svg";
+import Filter from "../../assets/filter.svg";
+import Order from "../../assets/order.svg";
+import Charge from "../../assets/charge.svg"
 import "./style.css";
 
 function Clients() {
@@ -22,6 +26,55 @@ function Clients() {
 					display='hidden'
 				/>
 			</navbar>
+
+			<div>
+				<header>
+					<p>Clientes</p>
+					<div className="id">
+						<div className="initials">LR</div>
+						<p>Lorena</p>
+					</div>
+				</header>
+
+				<div className="client-area">
+					<div className="client-area-header">
+						<div className="theme">
+							<img src={People} />
+							<h3>Clients</h3>
+						</div>
+						<div className="customer-interaction">
+							<button>+Adicionar cliente</button>
+							<img src={Filter}/>
+							<input 
+							placeholder="Pesquisa"
+							//imagem da lupa
+							/>
+						</div>
+					</div>
+					<div className="customer-registration">
+						<div className="line-with-titles">
+							<p><img src={Order}/>Cliente</p>
+							<p>CPF</p>
+							<p>E-mail</p>
+							<p>Telefone</p>
+							<p>Status</p>
+							<p>Criar Cobbrança</p>
+						</div>
+						<div className="clients">
+							<p>Sara da Silva</p>
+							<p>054 356 255 87</p>
+							<p>sarasilva@cubos.io</p>
+							<p>71 9 9462 8654</p>
+						</div>
+						<div className="status">Inadiplente</div>
+						<div className="charge">
+							<img src={Charge}/>
+							<p>Cobrança</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
 		</div>
 	);
 }
