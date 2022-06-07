e0bc6adbc9e2954563afa8b84bece2eea5453be3
Bruno <br9uno@gmail.com>
2022-06-07 16:14:36 -0300

    acerto

--- src/components/ClientsModal/index.jsx
+++ src/components/ClientsModal/index.jsx
@@ -1,198 +1,185 @@
-import People from "../../assets/people.svg";
-import Close from "../../assets/close.svg";
-import Inputs from "../Inputs";
-import api from "../../services/api"
-import './style.css';
 import { useState } from "react";
+import Close from "../../assets/close.svg";
+import People from "../../assets/people.svg";
+import api from "../../services/api";
+import Inputs from "../Inputs";
+import "./style.css";
 
 function ClientsModal({ handleClose }) {
-    const [form, setForm] = useState(
-        {
-            name: '',
-            email: '',
-            cpf: '',
-            phone: '',
-            cep: '',
-            address: '',
-            complement: '',
-            district: '',
-            city: '',
-            state: ''
-        });
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		cpf: "",
+		phone: "",
+		cep: "",
+		address: "",
+		complement: "",
+		district: "",
+		city: "",
+		state: "",
+	});
 
-    function handleChangeForm(e) {
-        const value = e.target.value;
-        setForm({
-            ...form,
-            [e.target.name]: value,
-        });
-    }
+	function handleChangeForm(e) {
+		const value = e.target.value;
+		setForm({
+			...form,
+			[e.target.name]: value,
+		});
+	}
 
+	async function handleSubmit(e) {
+		e.preventDefault();
+		if (!form.name || !form.email || !form.cpf || !form.phone) {
+			console.log("campos obrigatórios");
+			return;
+		}
+		console.log(form);
+		try {
+			const response = await api.post("/client", {
+				name: form.name,
+				email: form.email,
+				cpf: form.cpf,
+				phone: form.phone,
+				cep: form.cep,
+				address: form.address,
+				complement: form.complement,
+				district: form.district,
+				city: form.city,
+				state: form.state,
+			});
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-        if (!form.name || !form.email || !form.cpf || !form.phone) {
-            console.log('campos obrigatórios');
-            return;
-        }
-        console.log(form)
-        try {
-            const response = await api.post('/client',
-                {
-                    name: form.name,
-                    email: form.email,
-                    cpf: form.cpf,
-                    phone: form.phone,
-                    cep: form.cep,
-                    address: form.address,
-                    complement: form.complement,
-                    district: form.district,
-                    city: form.city,
-                    state: form.state
-                });
-            
-            console.log(response);
-        } catch (error) {
-            console.log(error.message);
-        }
+			console.log(response);
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
 
-    }
+	return (
+		<div className='register-clients'>
+			<div className='container'>
+				<div className='container-header'>
+					<div className='theme-modal'>
+						<img src={People} alt='' />
+						<h3>Cadastro do Cliente</h3>
+					</div>
+					<img src={Close} alt='' onClick={() => handleClose()} />
+				</div>
 
-    return (
-        <>
-         
-                <div className="register-clients">
-                    <div className="container">
-                        <div className="container-header">
-                            <div className="theme-modal">
-                                <img src={People} alt="" />
-                                <h3>Cadastro do Cliente</h3>
-                            </div>
-                            <img
-                                src={Close}
-                                alt=""
-                                onClick={() => handleClose()}
-                            />
-                        </div>
+				<form onSubmit={handleSubmit}>
+					<Inputs
+						type='text'
+						name='name'
+						label='Nome*'
+						id='name'
+						placeholder='Digite o nome'
+						style={{ width: "487px" }}
+						handleChangeForm={handleChangeForm}
+					/>
 
-                        <form onSubmit={handleSubmit}>
-                            <Inputs
-                                type="text"
-                                name="name"
-                                label="Nome*"
-                                id="name"
-                                placeholder="Digite o nome"
-                                style={{ width: "487px" }}
-                                handleChangeForm={handleChangeForm}
-                            />
+					<Inputs
+						type='email'
+						name='email'
+						label='E-mail*'
+						id='email'
+						placeholder='Digite seu e-mail'
+						style={{ width: "487px" }}
+						handleChangeForm={handleChangeForm}
+					/>
+					<div className='cpf-phone'>
+						<Inputs
+							type='text'
+							name='cpf'
+							label='CPF*'
+							id='cpf'
+							placeholder='Digite o CPF'
+							style={{ width: "235px" }}
+							handleChangeForm={handleChangeForm}
+						/>
 
-                            <Inputs
-                                type="email"
-                                name="email"
-                                label="E-mail*"
-                                id="email"
-                                placeholder="Digite seu e-mail"
-                                style={{ width: "487px" }}
-                                handleChangeForm={handleChangeForm}
-                            />
-                            <div className="cpf-phone">
-                                <Inputs
-                                    type="text"
-                                    name="cpf"
-                                    label="CPF*"
-                                    id="cpf"
-                                    placeholder="Digite o CPF"
-                                    style={{ width: "235px" }}
-                                    handleChangeForm={handleChangeForm}
+						<Inputs
+							type='text'
+							name='phone'
+							label='Telefone*'
+							id='phone'
+							placeholder='Digite o telefone'
+							style={{ width: "235px" }}
+							handleChangeForm={handleChangeForm}
+						/>
+					</div>
+					<Inputs
+						type='text'
+						name='address'
+						label='Endereço*'
+						id='address'
+						placeholder='Digite o endereço'
+						style={{ width: "487px" }}
+						handleChangeForm={handleChangeForm}
+					/>
 
-                                />
+					<Inputs
+						type='text'
+						name='complement'
+						label='Complemento*'
+						id='complement'
+						placeholder='Digite o complemento'
+						style={{ width: "487px" }}
+						handleChangeForm={handleChangeForm}
+					/>
 
-                                <Inputs
-                                    type="text"
-                                    name="phone"
-                                    label="Telefone*"
-                                    id="phone"
-                                    placeholder="Digite o telefone"
-                                    style={{ width: "235px" }}
-                                    handleChangeForm={handleChangeForm}
-                                />
-                            </div>
-                            <Inputs
-                                type="text"
-                                name="address"
-                                label="Endereço*"
-                                id="address"
-                                placeholder="Digite o endereço"
-                                style={{ width: "487px" }}
-                                handleChangeForm={handleChangeForm}
-                            />
+					<div className='cep-district'>
+						<Inputs
+							type='text'
+							name='cep'
+							label='CEP'
+							id='cep'
+							placeholder='Digite o CEP'
+							style={{ width: "235px" }}
+							handleChangeForm={handleChangeForm}
+						/>
 
-                            <Inputs
-                                type="text"
-                                name="complement"
-                                label="complement"
-                                id="complement"
-                                placeholder="Digite o complemento"
-                                style={{ width: "487px" }}
-                                handleChangeForm={handleChangeForm}
-                            />
+						<Inputs
+							type='text'
+							name='district'
+							label='Bairro'
+							id='district'
+							placeholder='Digite o bairro'
+							style={{ width: "235px" }}
+							handleChangeForm={handleChangeForm}
+						/>
+					</div>
 
+					<div className='city-uf'>
+						<Inputs
+							type='text'
+							name='city'
+							label='Cidade'
+							id='city'
+							placeholder='Digite a cidade'
+							style={{ width: "303px" }}
+							handleChangeForm={handleChangeForm}
+						/>
 
-                            <div className="cep-district">
-                                <Inputs
-                                    type="text"
-                                    name="cep"
-                                    label="CEP"
-                                    id="cep"
-                                    placeholder="Digite o CEP"
-                                    style={{ width: "235px" }}
-                                    handleChangeForm={handleChangeForm}
-                                />
+						<Inputs
+							type='text'
+							name='state'
+							label='UF'
+							id='uf'
+							placeholder='Digite a UF'
+							style={{ width: "160px" }}
+							handleChangeForm={handleChangeForm}
+						/>
+					</div>
 
-                                <Inputs
-                                    type="text"
-                                    name="district"
-                                    label="Bairro"
-                                    id="district"
-                                    placeholder="Digite o bairro"
-                                    style={{ width: "235px" }}
-                                    handleChangeForm={handleChangeForm}
-                                />
-
-                            </div>
-
-                            <div className="city-uf">
-                                <Inputs
-                                    type="text"
-                                    name="city"
-                                    label="Cidade"
-                                    id="city"
-                                    placeholder="Digite a cidade"
-                                    style={{ width: "303px" }}
-                                    handleChangeForm={handleChangeForm}
-                                />
-
-
-                                <Inputs
-                                    type="text"
-                                    name="state"
-                                    label="UF"
-                                    id="uf"
-                                    placeholder="Digite a UF"
-                                    style={{ width: "160px" }}
-                                    handleChangeForm={handleChangeForm}
-                                />
-                            </div>
-
-                            <div className="buttons">
-                                <button className="cancel">Cancelar</button>
-                                <button type="submit" className="apply">Aplicar</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-        </>
-    )
+					<div className='buttons'>
+						<button className='cancel'>Cancelar</button>
+						<button type='submit' className='apply'>
+							Aplicar
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default ClientsModal;
--- src/pages/Clients/index.jsx
+++ src/pages/Clients/index.jsx
@@ -1,25 +1,25 @@
-import DocumentImg from "../../assets/documents.svg";
-import HomeInactve from "../../assets/home-inactve.svg";
-import ClientsPink from "../../assets/clients-pink.svg";
-import NavLink from "../../components/NavLinks";
-import People from "../../assets/people.svg";
-import Filter from "../../assets/filter.svg";
-import Order from "../../assets/order.svg";
-import Charge from "../../assets/charge.svg";
-import "./style.css";
-import ClientsModal from "../../components/ClientsModal";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import api from "../../services/api";
-import Login from "../../components/Login";
-import ImgProfile from "../../assets/img-profile.png";
 import ArrowImg from "../../assets/arrow.svg";
-import ImgEdit from "../../assets/to-edit.svg";
+import Charge from "../../assets/charge.svg";
+import ClientsPink from "../../assets/clients-pink.svg";
+import DocumentImg from "../../assets/documents.svg";
 import ImgLogout from "../../assets/exit.svg";
-import Search from "../../assets/search.svg"
+import Filter from "../../assets/filter.svg";
+import HomeInactve from "../../assets/home-inactve.svg";
+import ImgProfile from "../../assets/img-profile.png";
+import Order from "../../assets/order.svg";
+import People from "../../assets/people.svg";
+import Lupa from "../../assets/search.svg";
+import ImgEdit from "../../assets/to-edit.svg";
+import ClientsModal from "../../components/ClientsModal";
+import Login from "../../components/Login";
+import NavLink from "../../components/NavLinks";
+import api from "../../services/api";
+import "./style.css";
 
 function Clients() {
-	const [openClientsModal, setOpenClientsModal] = useState(false)
+	const [openClientsModal, setOpenClientsModal] = useState(false);
 	const [clients, setClients] = useState();
 
 	useEffect(() => {
@@ -29,7 +29,7 @@ function Clients() {
 
 				setClients(response.data);
 			} catch (error) {
-				console.log(error.message)
+				console.log(error.message);
 			}
 		}
 		handleGetClients();
@@ -46,11 +46,7 @@ function Clients() {
 						display='hidden'
 					/>
 				</Link>
-				<NavLink
-					image={ClientsPink}
-					text='Clientes'
-					color='main-pink'
-				/>
+				<NavLink image={ClientsPink} text='Clientes' color='main-pink' />
 				<NavLink
 					image={DocumentImg}
 					text='Cobranças'
@@ -59,10 +55,10 @@ function Clients() {
 				/>
 			</nav>
 
-			<div className="centralize">
+			<div className='clients-container'>
 				<header>
 					<p>Clientes</p>
-					<div className="container-clients-margin">
+					<div className='container-clients-margin'>
 						<Login
 							image={ImgProfile}
 							imageArrow={ArrowImg}
@@ -70,39 +66,40 @@ function Clients() {
 							imageLogout={ImgLogout}
 						/>
 					</div>
-
 				</header>
 
-				<hr className="hr-clients" />
+				<hr className='hr-clients' />
 
-				<div className="client-area">
-					<div className="client-area-header">
-						<div className="theme">
-							<img src={People} alt="" />
+				<div className='client-area'>
+					<div className='clients-container-area-header'>
+						<div className='theme'>
+							<img src={People} alt='' />
 							<h3>Clientes</h3>
 						</div>
-						<div className="customer-interaction">
-							<button
-								onClick={() => setOpenClientsModal(true)}
-							>
+						<div className='customer-interaction'>
+							<button onClick={() => setOpenClientsModal(true)}>
 								+Adicionar cliente
 							</button>
-							<img src={Filter} alt="" />
-							<div className="img-insert">
-								<input placeholder="Pesquisa"/>
-								<img className="img-position" src={Search} />
+							<img src={Filter} alt='' />
+							<div className='clients-container-relative'>
+								<input placeholder='Pesquisa' />
+								<img src={Lupa} alt='' />
 							</div>
 						</div>
 					</div>
-					<div className="line-with-titles">
+
+					<div className='line-with-titles'>
 						<table>
 							<thead>
 								<tr>
-									<th className="center-order"><img src={Order} alt="" />Cliente</th>
+									<th className='center-order'>
+										<img src={Order} alt='' />
+										Cliente
+									</th>
 									<th>CPF</th>
 									<th>E-mail</th>
 									<th>Telefone</th>
-									<th >Status</th>
+									<th>Status</th>
 									<th>Criar Cobrança</th>
 								</tr>
 							</thead>
@@ -114,19 +111,19 @@ function Clients() {
 											<td>{cliente.cpf}</td>
 											<td>{cliente.email}</td>
 											<td>{cliente.phone}</td>
-											<td className="status">Inadiplente</td>
-											<td><img src={Charge} alt="" /></td>
+											<td className='status'>Inadiplente</td>
+											<td>
+												<img src={Charge} alt='' />
+											</td>
 										</tr>
 									</tbody>
 								))}
 						</table>
 					</div>
-					{openClientsModal && <ClientsModal
-						handleClose={() => setOpenClientsModal(false)}
-					/>}
-
+					{openClientsModal && (
+						<ClientsModal handleClose={() => setOpenClientsModal(false)} />
+					)}
 				</div>
-
 			</div>
 		</div>
 	);
